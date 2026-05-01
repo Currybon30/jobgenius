84245ea56df11c4709d4ffde16c3781e2ac6516b
Tuong Nguyen Pham <96371265+Currybon30@users.noreply.github.com>
2026-05-01 17:33:29 -0400

    Update job_recommender.py

---
+++ api_server/app/services/job_recommender.py
@@ -0,0 +1,4 @@
+# Show random jobs from Job API for unlogged-in users, matching the user's location and industry
+# If the user logged in and is a free user, show jobs matching the user's location, industry and job title
+# If the user logged in and is a premium user, show jobs matching the user's location, industry, job title, job level, etc.
+
