4ff1d12f49f0a1f611eeea0bb21addf71661edb1
Tuong Nguyen Pham <tuongnguyen2004danang@gmail.com>
2026-09-12 22:37:33 -0400

    Update user_helper.py
    implement get real user ip address function

--- api_server/app/helpers/user_helper.py
+++ api_server/app/helpers/user_helper.py
@@ -1,6 +1,8 @@
 import logging
 
+from fastapi import Request
 import niquests
+from app.core.config import settings
 
 logger = logging.getLogger(__name__)
 
@@ -29,6 +31,29 @@ async def get_user_city_and_country(ip_address: str | None = None):
 
     logger.error(f"Failed to get location info for IP: {ip_address}")
     return None, None, None
+
+
+def get_user_ip_address(request: Request):
+    if settings.DEBUG:
+        client_ip = request.headers.get("client-ip-address")
+        if client_ip:
+            return client_ip.strip()
+
+    # Cloudflare header: CF-Connecting-IP
+    cf_connecting_ip = request.headers.get("CF-Connecting-IP")
+    if cf_connecting_ip:
+        return cf_connecting_ip.strip()
+
+    # Reverse proxy / Load balancer
+    forwarded_for = request.headers.get("X-Forwarded-For")
+    if forwarded_for:
+        return forwarded_for.split(",")[0].strip()
+    
+    # Nginx / other reverse proxy
+    x_real_ip = request.headers.get("X-Real-IP")
+    if x_real_ip:
+        return x_real_ip.strip()
+    return request.client.host if request.client else None
 
 
 if __name__ == "__main__":
