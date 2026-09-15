8c2220947249e91fe7383060c5418a06335686b6
Tuong Nguyen Pham <tuongnguyen2004danang@gmail.com>
2026-09-15 15:11:08 -0400

    Update user.py

--- api_server/app/models/user.py
+++ api_server/app/models/user.py
@@ -5,8 +5,9 @@ from sqlalchemy import BigInteger, Column, DateTime, String
 class User(Base):
     __tablename__ = "users"
     uid = Column(BigInteger, primary_key=True, index=True)
-    plan = Column(String(50), nullable=False, default="FREE")
+    plan = Column(String(50), nullable=False, default="FREE", index=True)
     plan_expiry = Column(DateTime, nullable=True)
 
     def __repr__(self):
         return f"User(uid={self.uid}, plan='{self.plan}', plan_expiry={self.plan_expiry})"
+    
