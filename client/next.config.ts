7c1baf1bb1eda85314cc3dc96434d577d8cbe546
Tuong Nguyen Pham <tuongnguyen2004danang@gmail.com>
2026-09-17 09:09:51 -0400

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   // Allow phone testing over LAN (e.g. http://192.168.x.x:3000)
   allowedDevOrigins: ["192.168.0.79"],
   output: 'standalone',
+  devIndicators: false,
 };
 
 export default nextConfig;
